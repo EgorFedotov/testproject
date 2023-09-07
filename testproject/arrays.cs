1480d62742bf2385f68c38dd36588e29cf78f214
Egor Fedotov <egorfedotovarz@gmail.com>
2023-09-07 16:35:41 +0300

    arrays

---
+++ testproject/arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testproject
+{
+    internal class Arrays
+    {
+        static void Main(string[] args)
+        {
+            int[] cucubers = new int[10];
+
+            for (int i = 0; i < cucubers.Length; i++)
+            {
+                Console.WriteLine(cucubers[i]);
+            }
+
+        }
+    }
+}
