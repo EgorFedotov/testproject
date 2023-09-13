d19559f96fe5f9f606fb223c92277939fbedf621
Egor Fedotov <egorfedotovarz@gmail.com>
2023-09-13 09:07:59 +0300

    register for flight

--- testproject/arrays.cs
+++ testproject/arrays.cs
@@ -10,13 +10,29 @@ namespace testproject
     {
         static void Main(string[] args)
         {
-            int[] cucubers = new int[10];
+            int[] sectors = { 6, 28, 15, 15, 17 };
+            bool isOpen = true;
 
-            for (int i = 0; i < cucubers.Length; i++)
+            while(isOpen) 
             {
-                Console.WriteLine(cucubers[i]);
+                Console.SetCursorPosition(0, 18);
+                for(int i = 0; i < sectors.Length; i++)
+                {
+                    Console.WriteLine($"В секторе {i + 1} свободно {sectors[i]} мест.");
+                }
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Регистрация рейса");
+                Console.WriteLine("\n\n1 - забронировать места\n \n2 - выходи из программы.");
+                Console.WriteLine("Введите номер команды");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        isOpen = false;
+                        break;
+                }
             }
-
         }
     }
 }
